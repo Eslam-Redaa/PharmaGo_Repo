fccb8b9e5eabb3482c2e4b22e1aa2d564c7ef00e
Eslam-Redaa <islamsterling10@gmail.com>
2026-05-17 22:47:03 +0300

    some edits in app environment for cloudinary

--- src/main/java/com/demo/Controllers/MedicineController.java
+++ src/main/java/com/demo/Controllers/MedicineController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -32,7 +33,7 @@ public class MedicineController {
     // ================= ADD MEDICINE =================
     @PostMapping("/add")
     public ResponseEntity<AppResponse> addMedicine(
-            @RequestPart("data") MedicineRequest request,
+    			@ModelAttribute ("request") MedicineRequest request,
             @RequestPart(value = "image", required = false) MultipartFile image
     ) throws IOException {
         AppResponse response = medicineService.addMedicine(request, image);
--- src/main/java/com/demo/Controllers/PharmacyController.java
+++ src/main/java/com/demo/Controllers/PharmacyController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -32,7 +33,7 @@ public class PharmacyController {
     // ================= CREATE =================
     @PostMapping(consumes = {"multipart/form-data"})
     public ResponseEntity<AppResponse> createPharmacy(
-            @RequestPart("data") PharmacyRequest request,
+            @ModelAttribute ("request") PharmacyRequest request,
             @RequestPart(value = "image", required = false) MultipartFile image
     ) throws IOException {
 
